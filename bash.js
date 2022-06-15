ee762786ef49e3ae844083aa5e007a9d655ba0db
Ryan Scoville <ryanscoville29@yahoo.com>
2022-06-15 16:16:20 -0500

    completed nodemon

--- bash.js
+++ bash.js
@@ -1,18 +1,18 @@
 
+const pwdCommand = require('./pwd')
+
 process.stdout.write("prompt > ");
 
 process.stdin.on('data', (data) => {
     
     const cmd = data.toString().trim();
-
-    if (cmd === 'pwd') {
-        process.stdout.write(process.cwd());
-        process.stdout.write('\nprompt >');
-    }
-    else {
+    if(cmd !== 'pwd'){
         process.stdout.write("You typed: " + cmd);
         process.stdout.write('\nprompt >');
     }
+        
 });
+
+
 
 
