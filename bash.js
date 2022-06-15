a8f2a9b2b6314192eeafd98966990875af806b1b
Ryan Scoville <ryanscoville29@yahoo.com>
2022-06-15 16:44:56 -0500

    completed fs challenge

--- bash.js
+++ bash.js
@@ -1,12 +1,22 @@
 
 const pwdCommand = require('./pwd')
+ const fs = require('fs');
 
 process.stdout.write("prompt > ");
 
 process.stdin.on('data', (data) => {
     
     const cmd = data.toString().trim();
-    if(cmd !== 'pwd'){
+    if(cmd === 'ls'){
+        fs.readdir('./', 'utf8', (err, files)=>{
+            if(err){
+                throw err;
+            }   else{
+                process.stdout.write(files.join('\n'))
+                process.stdout.write('prompt >')
+            }
+        })
+    }else if(cmd !== 'pwd'){
         process.stdout.write("You typed: " + cmd);
         process.stdout.write('\nprompt >');
     }
---
+++ ls.js
@@ -0,0 +1,8 @@
+//  fs.readdir('./', 'utf8', (err, files)=>{
+//     if(err){
+//         throw err;
+//     }   else{
+//         process.stdout.write(files.join('\n'))
+//         process.stdout.write('prompt >')
+//     }
+// })
